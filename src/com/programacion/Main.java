1a97884f6142b928941d0ba96d3bd0bd61616c3a
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-01-12 11:08:02 +0100

    Modificaciones en la principal

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Rama principal");
+        System.out.println("Hago modificaciones en l arama rpincipal");
     }
 }
