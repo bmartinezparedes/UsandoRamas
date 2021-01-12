92964f7d14680703656df012b21d189278a7aa93
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-01-12 11:29:24 +0100

    Desplazo lineas para evitar conflicto

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
         System.out.println("Salgo de la rama principal");
         System.out.println("Voy a la bugFix");
     }
